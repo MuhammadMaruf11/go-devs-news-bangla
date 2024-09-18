61c52093977ccb3767105e1aeeaaeec31b0513e9
MuhammadMaruf11 <muhammad11maruf@gmail.com>
2024-09-18 23:41:30 +0600

    responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,52 +15,6 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 
 
-    // document.querySelectorAll('.mobile-main-menu > li > a').forEach(mainMenuItem => {
-    //     mainMenuItem.addEventListener('click', function (e) {
-    //         e.preventDefault();  // Prevent the default link behavior
-
-    //         // Toggle the open class on the clicked item
-    //         const parentLi = mainMenuItem.parentElement;
-    //         const isOpen = parentLi.classList.contains('open');
-    //         document.querySelectorAll('.mobile-main-menu > li').forEach(li => li.classList.remove('open'));
-
-    //         if (!isOpen) {
-    //             parentLi.classList.add('open');
-    //         }
-    //     });
-    // });
-
-
-    // document.querySelectorAll('.mobile-sub-menu > li > a').forEach(subMenuItem => {
-    //     subMenuItem.addEventListener('click', function (e) {
-    //         e.preventDefault();  // Prevent the default link behavior
-
-    //         // Toggle the open class on the clicked item
-    //         const parentLi = subMenuItem.parentElement;
-    //         const isOpen = parentLi.classList.contains('open');
-    //         document.querySelectorAll('.mobile-sub-menu > li').forEach(li => li.classList.remove('open'));
-
-    //         if (!isOpen) {
-    //             parentLi.classList.add('open');
-    //         }
-    //     });
-    // });
-
-    // /*-------------------------------------------
-    //      Sticky Header
-    //  --------------------------------------------- */
-
-    // let win = $(window);
-    // let sticky_id = $(".header-bottom");
-    // win.on('scroll', function () {
-    //     let scroll = win.scrollTop();
-    //     if (scroll < 245) {
-    //         sticky_id.removeClass("sticky-header");
-    //     } else {
-    //         sticky_id.addClass("sticky-header");
-    //     }
-    // });
-
 
     /*-------------------------------------------
          play video 
